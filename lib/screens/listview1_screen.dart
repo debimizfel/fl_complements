e399742ab20de85d1815615a54afb49517292394
debimizfel <debimizfel@gmail.com>
2022-05-16 13:55:32 -0400

    comments added

--- lib/screens/listview1_screen.dart
+++ lib/screens/listview1_screen.dart
@@ -1,22 +1,27 @@
+import 'package:fl_complements/themes/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class Listview1Screen extends StatelessWidget {
   const Listview1Screen({Key? key}) : super(key: key);
-  final optios = const ['Uno', 'Dos', 'Tres'];
+  final optios = const ['Ajustes', 'Perfil', 'Contactos'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Center(child: Text('Listview1Screen')),
+        title: const Text('Listview1Screen'),
       ),
       body: ListView(
         children: [
+          /* Operador spred extrae los elementos de la lista*/
           ...optios
               .map(
                 (object) => ListTile(
                   title: Text(object),
-                  trailing: const Icon(Icons.arrow_forward_ios_outlined),
+                  trailing: Icon(
+                    Icons.arrow_forward_ios_outlined,
+                    color: AppTheme.primary,
+                  ),
                 ),
               )
               .toList(),
